de2fed0ca5004fc192bae5d6a9fc9fb01657e63d
ZetaCygnus01 <122745586+ZetaCygnus01@users.noreply.github.com>
2025-05-21 20:44:01 +0000

    ComponenteFavoritos

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";  
 import { createStackNavigator } from "@react-navigation/stack";
+import { AppContext } from '../context/AppContext';
 
 import Aleatorios from "./src/componentes/aleatorios";
 import Capturados from "./src/componentes/capturados";
@@ -22,16 +23,27 @@ function ListaStack() {
   );
 }
 
+function FavoritosStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Favoritos" component={Favoritos} />
+      <Stack.Screen name="Pokemon" component={Pokemon} />
+    </Stack.Navigator>
+  );
+}
+
 export default function App() {
   return (
+    <AppProvider>
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Lista" component={ListaStack} />
         <Tab.Screen name="Aleatorios" component={Aleatorios} />
         <Tab.Screen name="Capturados" component={Capturados} />
-        <Tab.Screen name="Favoritos" component={Favoritos} />
+        <Tab.Screen name="Favoritos" component={FavoritosStack} />
         <Tab.Screen name="Usuario" component={Usuario} />
       </Tab.Navigator>
-    /</NavigationContainer>
+    </NavigationContainer>
+    </AppProvider>
   );
 }
